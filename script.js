c414de14308520039ad61a074f5363e5f6c4fe6a
Ritika027 <145582027+Ritika027@users.noreply.github.com>
2024-10-14 22:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('home-link').addEventListener('click', function() {
+    document.getElementById('home-section').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('events-link').addEventListener('click', function() {
+    document.getElementById('events-section').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('members-link').addEventListener('click', function() {
+    document.getElementById('members-section').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact-link').addEventListener('click', function() {
+    document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('about-link').addEventListener('click', function() {
+    document.getElementById('about-section').scrollIntoView({ behavior: 'smooth' });
+});
